fc4bab1605685294a6d545a1600223639497064b
JuleBehrens <56820567+JuleBehrens@users.noreply.github.com>
2021-05-20 13:03:51 +0200

    Add files via upload

---
+++ WLSB/src/main/java/com/SE3/WLSB/WlsbApplication.java
@@ -0,0 +1,13 @@
+package com.SE3.WLSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlsbApplication.class, args);
+	}
+
+}
---
+++ WLSB/src/test/java/com/SE3/WLSB/WlsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SE3.WLSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
