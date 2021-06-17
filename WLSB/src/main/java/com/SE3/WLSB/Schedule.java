5e01fb44a0172a1b503047953cecc19cdae4c8af
Jonah Schierding <jonah.schierding@live.de>
2021-06-17 16:13:23 +0200

    Refactoring

--- WLSB/src/main/java/com/SE3/WLSB/Schedule.java
+++ WLSB/src/main/java/com/SE3/WLSB/Schedule.java
@@ -17,6 +17,13 @@ public class Schedule {
     private final Duration napDuration = Duration.ofMinutes(20);
     private final int hoursOfSleepOver21 = 7;
     private final int hoursOfSleepUnder21 = 8;
+    private final String wakeUpString = "wakeup";
+    private final String workString = "work";
+    private final String freetimeString = "freetime";
+    private final String lunchString = "lunch";
+    private final String dinnerString = "dinner";
+    private final String napString = "nap";
+    private final String sleepString = "sleep";
     
     /**Status of schedule object*/
     int status;
@@ -95,29 +102,17 @@ public class Schedule {
     public String toString(){
         String schedule = "";
         if(status < 3){
-            schedule += String.format("{\"wakeup\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(wakeUpTime)); 
-            schedule += String.format("{\"work\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(morningWork));
-            if (morningFreetime != null){
-                schedule += String.format("{\"freetime\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(morningFreetime));
-            }
-			schedule += String.format("{\"lunch\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(lunchBreak));
-            if (nap){
-				schedule += String.format("{\"nap\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(napTime));
-			}
-            if(afternoonWork != null){
-                schedule += String.format("{\"work\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(afternoonWork));
-            }
-            if(afternoonFreetime != null){
-                schedule += String.format("{\"freetime\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(afternoonFreetime));
-            }
-            schedule += String.format("{\"dinner\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(dinner));
-            if(eveningWork != null){
-                schedule += String.format("{\"work\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(eveningWork));
-            }
-            if(eveningFreetime != null){
-                schedule += String.format("{\"freetime\": \"%s\"}​​​​​​​​​​​,", TimeParser.durationToString(eveningFreetime));
-            }
-            schedule += String.format("{\"sleep\": \"%s\"}​​​​​​​​​​​", TimeParser.durationToString(goToBed));
+            schedule = addDurationToSchedule(schedule, wakeUpString, wakeUpTime);
+            schedule = addDurationToSchedule(schedule, workString, morningWork);
+            schedule = addDurationToSchedule(schedule, freetimeString, morningFreetime);
+            schedule = addDurationToSchedule(schedule, lunchString, lunchBreak);
+			schedule = addDurationToSchedule(schedule, napString, napTime);
+            schedule = addDurationToSchedule(schedule, workString, afternoonWork);
+            schedule = addDurationToSchedule(schedule, freetimeString, afternoonFreetime);
+            schedule = addDurationToSchedule(schedule, dinnerString, dinner);
+            schedule = addDurationToSchedule(schedule, workString, eveningWork);
+            schedule = addDurationToSchedule(schedule, freetimeString, eveningFreetime);
+            schedule = addDurationToSchedule(schedule, sleepString, goToBed);
             return schedule;
         }else {
             return  "Schedule not possible";
@@ -125,60 +120,44 @@ public class Schedule {
  
     }
 
+    private String addDurationToSchedule(String schedule, String identifier, Duration duration){
+        if(duration != null){
+            schedule += String.format("{\"%s\": \"%s\"}​​​​​​​​​​​,", identifier, TimeParser.durationToString(duration));
+        }
+        return schedule;
+    }
+
     /**
     * determines a schedule and sets the attributes of the object to correct times
     */
     private void determineSchedule(){
-        
         Duration remainingWorkingHours = workingHours;
+        determineMorningWork(remainingWorkingHours);
+		determineLunchBreak();
+		remainingWorkingHours = determineMorningFreetime(remainingWorkingHours);
+		Duration afternoon = lunchBreak.plus(breaks);
+	    determineDinner(afternoon);
+		afternoon = determineNap(afternoon);
+		remainingWorkingHours = determineAfternoonActivities(remainingWorkingHours, afternoon);
+		Duration sleeptime = wakeUpTime.minus(sleep).plus(Duration.ofHours(24));
+        determineEveningActivities(remainingWorkingHours, sleeptime);
+    }
 
-        morningWork = wakeUpTime.plus(morningRoutine);
-
-		if (breakfast){
-			lunchBreak = morningWork.plus(Duration.ofHours(4));
-		}
-		else{
-			lunchBreak = morningWork.plus(Duration.ofHours(3));
-		}
-
-		if (lunchBreak.compareTo(Duration.ofHours(11)) <= 0){	// essen zu früh, dann frühestens 11:30 Mittag
-			lunchBreak = Duration.ofHours(11);
-		}
-		else if (lunchBreak.compareTo(Duration.ofHours(16)) >= 0){	// essen  nach 15:30  dann spätestens 15:30
-			lunchBreak = Duration.ofHours(16);
-		}
-
-		if (lunchBreak.minus(morningWork).compareTo(remainingWorkingHours) > 0){
-            morningFreetime = morningWork.plus(remainingWorkingHours);
-			remainingWorkingHours = Duration.ZERO;
+    private Duration determineMorningFreetime(Duration remainingWorkingHours) {
+        if (lunchBreak.minus(morningWork).compareTo(remainingWorkingHours) > 0){
+            if(morningFreetime == null){
+                morningFreetime = morningWork.plus(remainingWorkingHours);
+            }
+            remainingWorkingHours = Duration.ZERO;
 		}
 		else {
             morningFreetime = null;
 			remainingWorkingHours = remainingWorkingHours.minus(lunchBreak.minus(morningWork));
 		}
+        return remainingWorkingHours;
+    }
 
-		Duration afternoon = lunchBreak.plus(breaks);
-	    dinner = afternoon.plus(Duration.ofHours(5));
-
-		if (nap){
-            napTime = Duration.ofSeconds(afternoon.getSeconds());
-	    	afternoon = afternoon.plus(napDuration);
-		}
-
-		if (remainingWorkingHours.isZero()){
-            afternoonFreetime = Duration.ofSeconds(afternoon.getSeconds());
-            afternoonWork = null;
-		}
-		else {
-            afternoonWork = Duration.ofSeconds(afternoon.getSeconds());
-            afternoonFreetime = null;
-		    if (remainingWorkingHours.compareTo(Duration.ofHours(5)) < 0){
-                afternoonFreetime = afternoon.plus(remainingWorkingHours);
-			    remainingWorkingHours = Duration.ZERO;
-			}
-		}
-
-		Duration sleeptime = wakeUpTime.minus(sleep).plus(Duration.ofHours(24));
+    private void determineEveningActivities(Duration remainingWorkingHours, Duration sleeptime) {
         eveningWork = null;
         eveningFreetime = null;
 		if (!remainingWorkingHours.isZero()){
@@ -210,6 +189,60 @@ public class Schedule {
 
             goToBed = sleeptime;
 		}
+    }
+
+    private Duration determineAfternoonActivities(Duration remainingWorkingHours, Duration afternoon) {
+        if (remainingWorkingHours.isZero()){
+            afternoonFreetime = Duration.ofSeconds(afternoon.getSeconds());
+            afternoonWork = null;
+		}
+		else {
+            afternoonWork = Duration.ofSeconds(afternoon.getSeconds());
+            afternoonFreetime = null;
+		    if (remainingWorkingHours.compareTo(Duration.ofHours(5)) < 0){
+                afternoonFreetime = afternoon.plus(remainingWorkingHours);
+			    remainingWorkingHours = Duration.ZERO;
+			}
+		}
+        return remainingWorkingHours;
+    }
+
+    private Duration determineNap(Duration afternoon) {
+        if (nap){
+            napTime = Duration.ofSeconds(afternoon.getSeconds());
+	    	afternoon = afternoon.plus(napDuration);
+		}
+        return afternoon;
+    }
+
+    private void determineDinner(Duration afternoon) {
+        dinner = afternoon.plus(Duration.ofHours(5));
+    }
+
+    private void determineLunchBreak() {
+        if (breakfast){
+			lunchBreak = morningWork.plus(Duration.ofHours(4));
+		}
+		else{
+			lunchBreak = morningWork.plus(Duration.ofHours(3));
+		}
+
+		if (lunchBreak.compareTo(Duration.ofHours(11)) <= 0){	// essen zu früh, dann frühestens 11:30 Mittag
+			lunchBreak = Duration.ofHours(11);
+		}
+		else if (lunchBreak.compareTo(Duration.ofHours(16)) >= 0){	// essen  nach 15:30  dann spätestens 15:30
+			lunchBreak = Duration.ofHours(16);
+		}
+    }
+
+    private void determineMorningWork(Duration remainingWorkingHours) {
+        if(remainingWorkingHours.isZero()){
+            morningWork = null;
+            morningFreetime = wakeUpTime.plus(morningRoutine);
+        }
+        else {
+            morningWork = wakeUpTime.plus(morningRoutine);
+        }
     }
 
     /**
