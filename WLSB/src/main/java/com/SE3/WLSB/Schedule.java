8984146f467be4e9b1b0ae2bf87aa67d42f9f9e9
Jonah Schierding <jonah.schierding@live.de>
2021-06-23 15:20:14 +0200

    Comments and refactoring

--- WLSB/src/main/java/com/SE3/WLSB/Schedule.java
+++ WLSB/src/main/java/com/SE3/WLSB/Schedule.java
@@ -6,7 +6,9 @@ import java.time.Duration;
 /**
  * Class to model a schedule, objects cannot be changed
  * <br>
- * Through constructor a validation and determination of the schedule is triggered
+ * Build object through constructor {@link #Schedule(AppProperties properties, boolean nap, int age, boolean breakfast, String wakeUpTime, String morningRoutine, String workingHours)}
+ * <br>
+ * a validation and determination of the schedule can be triggered by {@link #determineSchedule()}
  * <br>
  * Schedule can be accessed via a method {@link #toString()} 
  * <br>
@@ -55,7 +57,6 @@ public class Schedule {
 	 * @param workingHours String (Format: "HH:mm"), Duration how long the person has to work on an average day
      */
     public Schedule(AppProperties properties, boolean nap, int age, boolean breakfast, String wakeUpTime, String morningRoutine, String workingHours){
-		this.status = 1;
         this.properties = properties;
         this.nap = nap;
         this.age = age;
@@ -66,31 +67,6 @@ public class Schedule {
         this.breaks = Duration.ofMinutes(properties.breakDurationInMinutes);
         this.napDuration = Duration.ofMinutes(properties.napDurationInMinutes); 
     }
-
-    /**
-     * sets status {@link #status} to 3: Determination not possible, if the sum of Durations of all events is longer than a day
-     */
-    private void checkIfActivitiesLongerThan24h() {
-        Duration wholeDuration = morningRoutine.plus(workingHours).plus(sleep).plus(breaks).plus(breaks);
-        if (nap){
-			wholeDuration = wholeDuration.plus(napDuration);
-		}
-        if (wholeDuration.compareTo(Duration.ofHours(24)) > 0 ){
-			status = 3;			// geht nicht
-		}
-    }
-
-    /**
-     * sets sleep {@link #sleep} dependent on age
-     */
-    private void determineSleepDuration() {
-		if (age > 21) {
-			sleep = Duration.ofHours(properties.hoursOfSleepOver21);
-		}
-		else {
-			sleep = Duration.ofHours(properties.hoursOfSleepUnder21);
-		}
-	}	
 
     /**
      * @return returns a String representation of the object in JSON-Format
@@ -116,6 +92,13 @@ public class Schedule {
  
     }
 
+    /**
+     * Adds a Duration with an identifier as key in JSON at the end of a given String 
+     * @param schedule String to be extended
+     * @param identifier Identifier for json key
+     * @param duration Duration to be added
+     * @return schedule-String extended with the identifier and Duration
+     */
     private String addDurationToSchedule(String schedule, String identifier, Duration duration){
         if(duration != null){
             schedule += String.format("{\"%s\":\"%s\"},", identifier, TimeParser.durationToString(duration));
@@ -124,9 +107,13 @@ public class Schedule {
     }
 
     /**
-    * determines a schedule and sets the attributes of the object to correct times
+    * determines a schedule, if never called before, and sets the attributes of the object to correct times and status
     */
     public int determineSchedule(){
+        if(status != 0){
+            return status;
+        }
+        status = 1;
         determineSleepDuration();
         checkIfActivitiesLongerThan24h();
         if(status == 3){
@@ -145,6 +132,72 @@ public class Schedule {
         return status;
     }
 
+    /**
+     * sets sleep {@link #sleep} dependent on age
+     */
+    private void determineSleepDuration() {
+		if (age > 21) {
+			sleep = Duration.ofHours(properties.hoursOfSleepOver21);
+		}
+		else {
+			sleep = Duration.ofHours(properties.hoursOfSleepUnder21);
+		}
+	}	
+
+     /**
+     * sets status {@link #status} to 3: Determination not possible, if the sum of Durations of all events is longer than a day
+     */
+    private void checkIfActivitiesLongerThan24h() {
+        Duration wholeDuration = morningRoutine.plus(workingHours).plus(sleep).plus(breaks).plus(breaks);
+        if (nap){
+			wholeDuration = wholeDuration.plus(napDuration);
+		}
+        if (wholeDuration.compareTo(Duration.ofHours(24)) > 0 ){
+			status = 3;			// geht nicht
+		}
+    }
+
+    /**
+     * determines if working in the morning is needed and sets {@link #morningWork} or {@link #morningFreetime}
+     * @param remainingWorkingHours working hours that have not be scheduled yet
+     */
+    private void determineMorningWork(Duration remainingWorkingHours) {
+        if(remainingWorkingHours.isZero()){
+            morningFreetime = wakeUpTime.plus(morningRoutine);
+        }
+        else {
+            morningWork = wakeUpTime.plus(morningRoutine);
+        }
+    }
+
+    /**
+     * Determines when lunch is scheduled, dependent on {@link #breakfast}, lunch is always between 11:00 and 16:00 
+     * <br>
+     * Normally lunch is scheduled a fix amount of time (as specified from properties{@link #properties}) after the morningRoutine 
+     */
+    private void determineLunchBreak() {
+        Duration morning = wakeUpTime.plus(morningRoutine);
+        if (breakfast){
+			lunchBreak = morning.plus(Duration.ofHours(properties.morningWorkWithBreakfastInHours));
+		}
+		else{
+			lunchBreak = morning.plus(Duration.ofHours(properties.morningWorkWithoutBreakfastInHours));
+		}
+
+		if (lunchBreak.compareTo(Duration.ofHours(11)) <= 0){	// essen zu früh, dann frühestens 11:30 Mittag
+			lunchBreak = Duration.ofHours(11);
+		}
+		else if (lunchBreak.compareTo(Duration.ofHours(16)) >= 0){	// essen  nach 15:30  dann spätestens 15:30
+			lunchBreak = Duration.ofHours(16);
+		}
+    }
+
+    /**
+     * Determines if freetime before lunch is possible and if yes sets {@link #morningFreetime}
+     * <br>
+     * @param remainingWorkingHours working hours that have not be scheduled yet
+     * @return new amount of working hours that have not be scheduled yet
+     */
     private Duration determineMorningFreetime(Duration remainingWorkingHours) {
         if (lunchBreak.minus(wakeUpTime.plus(morningRoutine)).compareTo(remainingWorkingHours) > 0){
             if(morningFreetime == null){
@@ -159,6 +212,56 @@ public class Schedule {
         return remainingWorkingHours;
     }
 
+    /**
+     * Determines if a nap hast to be scheduled and schedules it if necessary {@link #napTime}
+     * @param afternoon marks the beginning of the afternoon, after lunchbreak
+     * @return beginning of afternoon, if nap is scheduled, afternoon is later
+     */
+    private Duration determineNap(Duration afternoon) {
+        if (nap){
+            napTime = Duration.ofSeconds(afternoon.getSeconds());
+	    	afternoon = afternoon.plus(napDuration);
+		}
+        return afternoon;
+    }
+
+    /**
+     * Determines {@link #dinner} a fix amount of time (as specified by {@link #properties}) after lunckbreak
+     * @param afternoon marks the beginning of the afternoon, after lunchbreak
+     */
+    private void determineDinner(Duration afternoon) {
+        dinner = afternoon.plus(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours));
+    }
+
+    /**
+     * Determines which activities during afternoon are scheduled ({@link #afternoonFreetime} and {@link #afternoonWork})
+     * @param remainingWorkingHours working hours that have not be scheduled yet
+     * @param afternoon marks the beginning of the afternoon, after lunchbreak
+     * @return new amount of working hours that have not be scheduled yet
+     */
+    private Duration determineAfternoonActivities(Duration remainingWorkingHours, Duration afternoon) {
+        if (remainingWorkingHours.isZero()){
+            afternoonFreetime = Duration.ofSeconds(afternoon.getSeconds());
+		}
+		else {
+            afternoonWork = Duration.ofSeconds(afternoon.getSeconds());
+		    if (remainingWorkingHours.compareTo(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours)) <= 0){
+                if (remainingWorkingHours.compareTo(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours)) < 0){
+                    afternoonFreetime = afternoon.plus(remainingWorkingHours);
+                }
+			    remainingWorkingHours = Duration.ZERO;
+			}else {
+                remainingWorkingHours = remainingWorkingHours.minus(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours));
+            }
+		}
+        return remainingWorkingHours;
+    }
+
+    /**
+     * Determines which activities during evening are scheduled and when to go to bed ({@link #eveningFreetime}, {@link #eveningWork} and {@link #goToBed})
+     * @param remainingWorkingHours working hours that have not be scheduled yet
+     * @param sleeptime Duration how long sleep has to be scheduled
+     */
     private void determineEveningActivities(Duration remainingWorkingHours, Duration sleeptime) {
 		if (!remainingWorkingHours.isZero()){
             eveningWork = dinner.plus(breaks);
@@ -189,62 +292,6 @@ public class Schedule {
 
             goToBed = sleeptime;
 		}
-    }
-
-    private Duration determineAfternoonActivities(Duration remainingWorkingHours, Duration afternoon) {
-        if (remainingWorkingHours.isZero()){
-            afternoonFreetime = Duration.ofSeconds(afternoon.getSeconds());
-		}
-		else {
-            afternoonWork = Duration.ofSeconds(afternoon.getSeconds());
-		    if (remainingWorkingHours.compareTo(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours)) <= 0){
-                if (remainingWorkingHours.compareTo(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours)) < 0){
-                    afternoonFreetime = afternoon.plus(remainingWorkingHours);
-                }
-			    remainingWorkingHours = Duration.ZERO;
-			}else {
-                remainingWorkingHours = remainingWorkingHours.minus(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours));
-            }
-		}
-        return remainingWorkingHours;
-    }
-
-    private Duration determineNap(Duration afternoon) {
-        if (nap){
-            napTime = Duration.ofSeconds(afternoon.getSeconds());
-	    	afternoon = afternoon.plus(napDuration);
-		}
-        return afternoon;
-    }
-
-    private void determineDinner(Duration afternoon) {
-        dinner = afternoon.plus(Duration.ofHours(properties.durationBetweenLunchAndDinnerInHours));
-    }
-
-    private void determineLunchBreak() {
-        Duration morning = wakeUpTime.plus(morningRoutine);
-        if (breakfast){
-			lunchBreak = morning.plus(Duration.ofHours(properties.morningWorkWithBreakfastInHours));
-		}
-		else{
-			lunchBreak = morning.plus(Duration.ofHours(properties.morningWorkWithoutBreakfastInHours));
-		}
-
-		if (lunchBreak.compareTo(Duration.ofHours(11)) <= 0){	// essen zu früh, dann frühestens 11:30 Mittag
-			lunchBreak = Duration.ofHours(11);
-		}
-		else if (lunchBreak.compareTo(Duration.ofHours(16)) >= 0){	// essen  nach 15:30  dann spätestens 15:30
-			lunchBreak = Duration.ofHours(16);
-		}
-    }
-
-    private void determineMorningWork(Duration remainingWorkingHours) {
-        if(remainingWorkingHours.isZero()){
-            morningFreetime = wakeUpTime.plus(morningRoutine);
-        }
-        else {
-            morningWork = wakeUpTime.plus(morningRoutine);
-        }
     }
 
     /**
